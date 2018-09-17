f2588e5fb2d05c1a05cd1919074420f91c62887f
Kazuki Kishishita <kishi-tennis0310@rakuten.jp>
2018-09-17 15:42:44 +0800

    新規登録

--- RealmTodo/ViewController.swift
+++ RealmTodo/ViewController.swift
@@ -1,26 +1,29 @@
-//
-//  ViewController.swift
-//  RealmTodo
-//
-//  Created by 岸下和樹 on 2018/09/17.
-//  Copyright © 2018年 岸下和樹. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+//    行数を決める
+    func tableview(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
+    
+//    セルの中身を決める
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "タスク"
+        return cell
+    }
 
 }
 
